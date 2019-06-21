24c98f5851515f4f9d9e92064ca6856eb3528622
HYXBio <45580758+HYXBio@users.noreply.github.com>
2019-06-21 21:02:39 +0800

    Revert "Revert "order""
    This reverts commit be831a1f9e97f3ce345ed634701c7f9e336eb7d7.

--- team_work/src/main/java/com/my_shop/service/OrderService.java
+++ team_work/src/main/java/com/my_shop/service/OrderService.java
@@ -3,5 +3,5 @@ package com.my_shop.service;
 import com.alibaba.fastjson.JSONObject;
 
 public interface OrderService {
-    public JSONObject orderGeneration(Integer[] ids,Integer address_id);
+    public JSONObject handlerGenerationOrder(Integer[] ids,Integer address_id,Integer customer_id);
 }
