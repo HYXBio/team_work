e5fc072a00f73745c06d1d2cca07c18a3462e096
HYXBio <45580758+HYXBio@users.noreply.github.com>
2019-06-23 10:31:50 +0800

    地址删除

--- team_work/src/main/java/com/my_shop/service/AddressService.java
+++ team_work/src/main/java/com/my_shop/service/AddressService.java
@@ -1,5 +1,6 @@
 package com.my_shop.service;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.my_shop.entity.Address;
 
@@ -17,4 +18,11 @@ public interface AddressService {
      * @return
      */
     public JSONObject upDateOrAddAddress(Address address);
+
+    /**
+     * 删除地址信息根据地址id
+     * @param id
+     * @return
+     */
+    public JSONObject deleteAddressById(Integer id);
 }
