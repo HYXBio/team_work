7e6325a005917da0c9ace1549f88a5712653b07d
kbrkl <1277278917@qq.com>
2019-06-23 16:08:52 +0800

    Update commodity_show.js

--- team_work/src/main/webapp/js/commodity/commodity_show.js
+++ team_work/src/main/webapp/js/commodity/commodity_show.js
@@ -70,7 +70,7 @@ function handleTb_CommodityList(list) {
         '                    <div class="send"><small>配送至：</small><input value="北京朝阳区三环以内" name="" /><span>由 <u>奥凡尼旗舰店</u>从广东佛山市发货，并提供售后服务。</span><div class="clear"></div></div>\n' +
         '                    <div class="size"><small>选择尺码：</small><p><span>进口厚皮 双人位+贵妃位+边几</span><span class="on">进口厚皮 双人位+贵妃位+边几</span><span>进口厚皮 全套系列</span><span>进口厚皮 双+贵+单+边几</span></p><div class="clear"></div></div>\n' +
         '                    <div class="number">\n' +
-        '                        <small>数量：</small><input type="text" class="num" id="commodity_number"/><span class="add"></span><span class="minus"></span>\n' +
+        '                        <small>数量：</small><input type="text" class="num" value="1" id="commodity_number"/><span class="add"></span><span class="minus"></span>\n' +
         '                        <div class="ku"><p>件&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;库存<span>&tb_CommodityListIn_stock</span>件</p></div>\n' +
         '                        <div class="clear"></div>\n' +
         '                    </div>\n' +
